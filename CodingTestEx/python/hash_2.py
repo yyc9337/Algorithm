ff187e23d9bb2a9ab0c16d4befe63d80522f1a52
유영찬 <yyk7009@innerwave.co,kr>
2021-10-06 15:43:02 +0900

    hash 2번 미완성

--- CodingTestEx/python/hash_2.py
+++ CodingTestEx/python/hash_2.py
@@ -1,10 +1,21 @@
 phone_book = ["119", "97674223", "1195524421"]
 
-print(phone_book[0] == phone_book[2])
+
+aa = phone_book[1].find(phone_book[0])
+
+print(aa)
 
 
 def solution(phone_book):
+    i = 0
+    for i in range(len(phone_book)) :
+        if phone_book[i].find(phone_book[0]) == 0 :
+            return False
+        else  :
+            return True 
+        
     answer = True
 
-
     return answer
+
+print(solution(phone_book))
