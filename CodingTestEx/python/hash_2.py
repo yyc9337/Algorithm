2874abebbce913ff19f435e3ca01055f0c297ddc
유영찬 <yyk7009@innerwave.co.kr>
2021-10-13 23:33:25 +0900

    hash 2번

--- CodingTestEx/python/hash_2.py
+++ CodingTestEx/python/hash_2.py
@@ -1,10 +1,15 @@
 phone_book = ["119", "97674223", "1195524421"]
 
-print(phone_book[0] == phone_book[2])
+
+for i in range(len(phone_book)) :
+    i = 1    
+    if phone_book[i].find(phone_book[0]) == -1 :
+        print(True)
+    else : 
+        print(False)  
 
 
-def solution(phone_book):
-    answer = True
+
+        
 
 
-    return answer
